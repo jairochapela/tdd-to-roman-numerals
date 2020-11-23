0f14f430f4db918301c0c2ffca0bc7a5d7d12eec
Aula <Aula@EQUIP079574.xunta.local>
2020-11-23 13:56:30 +0100

    Usando bucles y arrays.

--- index.js
+++ index.js
@@ -7,61 +7,16 @@
  */
 export function toRoman(n) {
 
+  const cantidades = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const letras = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
   let resultado = "";
 
-  while (n>=1000) {
-    resultado += "M";
-    n -= 1000;
+  for (let i = 0; i < cantidades.length; i++) {
+    while (n>=cantidades[i]) {
+      resultado += letras[i];
+      n -= cantidades[i];
+    }    
   }
-  if (n>=900) {
-    resultado += "CM";
-    n -= 900;
-  }
-  if (n>=500) {
-    resultado += "D";
-    n -= 500;
-  }  
-  if (n>=400) {
-    resultado += "CD";
-    n -= 400;
-  }
-  while (n>=100) {
-    resultado += "C";
-    n -= 100;
-  }
-  if (n>=90) {
-    resultado += "XC";
-    n -= 90;
-  }
-  if (n>=50) {
-    resultado += "L";
-    n -= 50;
-  }
-  if (n>=40) {
-    resultado += "XL";
-    n -= 40;
-  }
-  while (n>=10) {
-    resultado += "X";
-    n -= 10;
-  }
-  if (n==9) {
-    resultado += "IX";
-    n -= 9;
-  }
-  if (n==4) {
-    resultado += "IV";
-    n -= 4;
-  }
-  if (n>=5) {
-    resultado += "V";
-    n -= 5;
-  }
-
-  for (let i = 0; i < n; i++) {
-    resultado += "I"; 
-  }
-
 
   return resultado;
 }
