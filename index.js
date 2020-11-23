25ab38a40372f12f946da2f74198354ed395c478
Aula <Aula@EQUIP079574.xunta.local>
2020-11-23 11:34:57 +0100

    Casos de prueba entre 1 y 9

--- index.js
+++ index.js
@@ -6,7 +6,28 @@
  * @returns {string} El equivalente en números romanos.
  */
 export function toRoman(n) {
-  return "I";
+
+  let resultado = "";
+
+  if (n==9) {
+    resultado = "IX";
+    n -= 9;
+  }
+  else if (n==4) {
+    resultado = "IV";
+    n -= 4;
+  }
+  else if (n>=5) {
+    resultado += "V";
+    n -= 5;
+  }
+
+  for (let i = 0; i < n; i++) {
+    resultado += "I"; 
+  }
+
+
+  return resultado;
 }
 
 
